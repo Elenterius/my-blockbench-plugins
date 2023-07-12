c8886a23432a42e0faeef8d165600911c219b860
Elenterius <w.elenterius@gmail.com>
2023-07-12 20:46:29 +0200

    feat: add plugin template

---
+++ plugins/template.js
@@ -0,0 +1,33 @@
+/**
+ * @author Elenterius
+ * @link https://github.com/Elenterius
+ * @copyright Elenterius 2023
+ * @license MIT
+ */
+"use strict";
+
+(function () {
+    'use strict';
+    const PLUGIN_ID = "template";
+
+    BBPlugin.register(PLUGIN_ID, {
+        title: 'Template',
+        creation_date: "2023-07-10",
+        author: 'Elenterius',
+        description: 'Template',
+        about: "outdated, use about.md instead"
+        icon: 'extension',
+        tags: ["Minecraft"],
+        version: '0.1.0',
+        min_version: "4.5.0",
+        max_version: "5.0.0",
+        new_repository_format: true,
+        variant: 'both',
+
+        onload() {},
+        onunload() {},
+        oninstall() {},
+		onuninstall() {}
+    });
+
+})();
